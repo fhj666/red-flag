e40ef7f70e146e9a1d3b6975194e6fde10fe18a7
fhj666 <2920288394@qq.com>
2021-10-30 09:15:41 +0800

    '1654616415656'

--- js/header.js
+++ js/header.js
@@ -10,6 +10,19 @@ icon_cuo.addEventListener("click", function () {
     menu.style.top = "-700px";
     body.style.overflow = "visible";
 })
+function setRem() {
+    var ui_w = 375;
+    // 获取屏幕的宽度
+    var clientWidth = document.documentElement.clientWidth || document.body.clientWidth;
+    console.log(ui_w, clientWidth);
+
+    // 通过js动态改变html根节点字体大小
+    var html_ = document.getElementsByTagName('html')[0];
+    html_.style.fontSize = (clientWidth / ui_w) * 100 + 'px';
+}
+// window.onresize 浏览器被重置大小执行事件
+window.onresize = setRem;
+
 $(function () {
     $(".menu_body_lis").click(function () {
         $(".menu_body_lis_ol").slideUp(function () {
